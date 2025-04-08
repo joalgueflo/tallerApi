6e856a90b1b6d1709342559fe068abd9fb2ed1d5
José Guerrero <A01285612@tec.mx>
2025-04-08 22:34:00 +0000

    Termino la clase

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
@@ -11,6 +11,10 @@ app = Flask(__name__)
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+
+@app.route('/', methods=['GET'])
+def root():
+    return "Hello World"
 
 @app.route('/test-connection', methods=['GET'])
 def test_connection():
@@ -25,6 +29,41 @@ def test_connection():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
+@app.route('/peliculas', methods=['GET'])
+def get_peliculas():
+    try:
+        response = supabase.table('peliculas').select('*').execute() # query de postgres a nuestra db
+        return jsonify({"peliculas": response.data}), 200 
+    except Exception as e:
+        return jsonify({"error": str(e)}) , 500
+        
+
+@app.route("/peliculas", methods= ["POST"])
+def agregar_pelicula():
+    data = request.get_json()
+
+    # Validación básica
+    nombre = data.get('nombre')
+    duracion = data.get('duracion')
+    director = data.get('director')
+    releaseYear = data.get('releaseYear')
+
+    if not all([nombre, duracion, director, isinstance(releaseYear, int)]):
+        return jsonify({'error': 'Datos inválidos'}), 400
+
+    nueva_pelicula = {
+        'nombre': nombre,
+        'duracion': duracion,
+        'director': director,
+        'releaseYear': releaseYear
+    }
     
+    response = supabase.table("peliculas").insert(nueva_pelicula).execute()
+
+    # Aquí podrías guardar la película en una base de datos
+    return jsonify({'mensaje': 'Película agregada', 'pelicula': nueva_pelicula}), 201
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
