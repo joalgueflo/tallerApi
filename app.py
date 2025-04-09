32400fa422443933ff99b56e6541852e8d092072
José Guerrero <A01285612@tec.mx>
2025-04-09 19:34:00 +0000

    Finished basic endpoints

--- app.py
+++ app.py
@@ -43,19 +43,21 @@ def agregar_pelicula():
     data = request.get_json()
 
     # Validación básica
-    nombre = data.get('nombre')
-    duracion = data.get('duracion')
     director = data.get('director')
     releaseYear = data.get('releaseYear')
+    name = data.get('name')
+    imageURL = data.get('imageURL')
+    duration = data.get('duration')
 
-    if not all([nombre, duracion, director, isinstance(releaseYear, int)]):
+    if not all([name, duration, director, isinstance(releaseYear, int)]):
         return jsonify({'error': 'Datos inválidos'}), 400
 
     nueva_pelicula = {
-        'nombre': nombre,
-        'duracion': duracion,
+        'name': name,
+        'duration': duration,
         'director': director,
-        'releaseYear': releaseYear
+        'releaseYear': releaseYear,
+        'imageURL': imageURL
     }
     
     response = supabase.table("peliculas").insert(nueva_pelicula).execute()
@@ -63,7 +65,29 @@ def agregar_pelicula():
     # Aquí podrías guardar la película en una base de datos
     return jsonify({'mensaje': 'Película agregada', 'pelicula': nueva_pelicula}), 201
 
+@app.route("/peliculas/<id>", methods = ["GET"])
+def get_pelicula(id):
+    try:
+        response = supabase.table('peliculas').select('*').eq('id', id).execute()
+        if response.data:
+            return jsonify({"pelicula": response.data[0]}), 200
+        else:
+            return jsonify({"error": "Pelicula no encontrada"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
+
+@app.route("/peliculas/<id>", methods = ["DELETE"])
+def eliminar_pelicula(id):
+    try:
+        response = supabase.table('peliculas').delete().eq('id', id).execute()
+        if response.data:
+            return jsonify({"mensaje": "Pelicula eliminada"}), 200
+        else:
+            return jsonify({"error": "Pelicula no encontrada"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 if __name__ == '__main__':
     app.run(debug=True)
